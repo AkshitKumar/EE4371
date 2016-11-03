b0b589d65a4e8a2f4b3bc4c11924bbd29b002d02
AkshitKumar <akshitkumar100@gmail.com>
2016-11-03 15:26:52 +0530

    Removed unnecessary comments

--- take-home-midsem-2/midsem-solution-3/midsem-solution-3.c
+++ take-home-midsem-2/midsem-solution-3/midsem-solution-3.c
@@ -235,43 +235,6 @@ void drop_stale_video_packets(){
     }
 }
 
-/*
-void send_video_packet(){
-    current_time += t_next_video_dequeue;
-    packet = dequeue_video_packet();
-    printf("%lf\t%s\t%s\n",current_time,"video","SENT");
-    t_next_enqueue -= t_next_video_dequeue;
-    if(!video_queue_empty())
-        t_next_video_dequeue = ((double)video_queue[video_front].size)/64000.0;
-}
-
-void send_http_packet(){
-    current_time += t_next_http_dequeue;
-    packet = dequeue_http_packet();
-    printf("%lf\t%s\t%s\n",current_time,"http","SENT");
-    t_next_enqueue -= t_next_http_dequeue;
-    if(!http_queue_empty())
-        t_next_http_dequeue = ((double)http_queue[http_front].size)/64000.0;
-}
-
-void receive_http_packet(){
-    current_time += t_next_enqueue;
-    t_next_http_dequeue -= t_next_enqueue;
-    enqueue_http_packet(packets[i]);
-    count_http_inserted++;
-    t_next_enqueue = packets[++i].t_arrival;
-    t_next_enqueue -= current_time;
-}
-
-void receive_video_packet(){
-    current_time += t_next_enqueue;
-    t_next_video_dequeue -= t_next_enqueue;
-    enqueue_video_packet(packets[i]);
-    count_video_inserted++;
-    t_next_enqueue = packets[++i].t_arrival;
-    t_next_enqueue -= current_time;
-}
-*/
 int main(int argc,char** argv){
     if(argc != 2){
         printf("Usage ./a.out <filename>");
@@ -300,36 +263,6 @@ int main(int argc,char** argv){
         }
         drop_stale_http_packets();
         drop_stale_video_packets();
-        /*
-        if(!http_queue_empty()){
-            http_packet = http_queue[http_front];
-            do{
-                is_http_dropped = false;
-                if(fabs(current_time - http_packet.t_arrival) > 15.000000){
-                    is_http_dropped = true;
-                    http_packet = dequeue_http_packet();
-                    count_http_drop++;
-                    printf("%lf\t%s\t%s\n",current_time,"http","DROP");
-                    if(!http_queue_empty())
-                        t_next_http_dequeue = ((double)http_queue[http_front].size)/64000.0;
-                }
-            }while(is_http_dropped && !http_queue_empty());
-        }
-        if(!video_queue_empty()){
-            video_packet = video_queue[video_front];
-            do{
-                is_video_dropped = false;
-                if(fabs(current_time - video_packet.t_arrival) > 1.000000){
-                    is_video_dropped = true;
-                    video_packet = dequeue_video_packet();
-                    count_video_drop++;
-                    printf("%lf\t%s\t%s\n",current_time,"video","DROP");
-                    if(!video_queue_empty())
-                        t_next_video_dequeue = ((double)video_queue[video_front].size)/64000.0;
-                }
-            }while(is_video_dropped && !video_queue_empty());
-        }
-        */
         if(http_queue_empty() && !video_queue_empty()){
             if(t_next_video_dequeue < t_next_enqueue){
                 current_time += t_next_video_dequeue;
@@ -369,7 +302,6 @@ int main(int argc,char** argv){
                 printf("%lf\t%s\t%s\n",current_time,"http","SENT");
                 if(!http_queue_empty()){
                     t_next_http_dequeue = ((double)http_queue[http_front].size)/64000.0;
-                    //printf("%lf\n",t_next_http_dequeue);
                 }
             }
             else if(t_next_enqueue <= t_next_http_dequeue){
