5e8bed7e356764753e58ae2ecced63fa1d432bc1
AkshitKumar <akshitkumar100@gmail.com>
2016-11-27 15:00:31 +0530

    Greedy Solution done

--- endsem/knapsack/knapsack.c
+++ endsem/knapsack/knapsack.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <stdbool.h>
 #include <string.h>
+#include <limits.h>
 
 #define max(a,b) (a > b ? a : b)
 
@@ -16,7 +17,12 @@ Cost dp[2000][2000];
 
 int W;
 int weights[2000];
-int knapsack_elements[2000];
+int knapsack_elements_dp[2000];
+int knapsack_elements_greedy[2000];
+int num_elements = 0;
+int compare_function(const void *a, const void *b){
+	return ( *(int*)b - *(int*)a);
+}
 
 int get_limit(char line[]){
 	char *pos;
@@ -42,6 +48,7 @@ int get_weights(char line[]){
 	token = strtok(line," ");
 	int num;
 	int i = 1;
+	weights[i++] = INT_MAX;
 	while(token != NULL){
 		sscanf(token,"%d",&num);
 		weights[i++] = num;
@@ -80,7 +87,7 @@ int get_knapsack_elements(int W,int n){
 	int num_elements = 0;
 	while(i > 0){
 		if(dp[i][line].weight > dp[i-1][line].weight){
-			knapsack_elements[num_elements++] = weights[i];
+			knapsack_elements_dp[num_elements++] = weights[i];
 			line = line - weights[i];
 			i--;
 		}
@@ -89,6 +96,26 @@ int get_knapsack_elements(int W,int n){
 		}
 	}
 	return num_elements;
+}
+
+int greedy_knapsack_solution(int W,int n){
+	qsort(weights,n,sizeof(int),compare_function);
+	num_elements = 0;
+	int i = 1;
+	int sum = 0;
+	while(i <= n){
+		sum += weights[i];
+		num_elements++;
+		if((sum <= (float)W - log(num_elements))){
+			knapsack_elements_greedy[num_elements] = weights[i];
+		}
+		else{
+			sum -= weights[i];
+			num_elements--;
+		}
+		i++;
+	}
+	return sum;
 }
 
 void print_dp_matrix(int W, int n){
@@ -100,16 +127,18 @@ void print_dp_matrix(int W, int n){
 	}
 }
 
-void printWeights(int num_weights){
-	for(int i = 0; i < num_weights; i++){
-		printf("%d ",weights[i]);
+void print_knapsack_elements_dp(int N){
+	for(int i = 0; i < N ; i++){
+		printf("%d ",knapsack_elements_dp[i]);
 	}
 	printf("\n");
 }
 
-void print_knapsack_elements(int N){
-	for(int i = 0; i < N ; i++){
-		printf("%d ",knapsack_elements[i]);
+void print_knapsack_elements_greedy(int N){
+	for(int i = 1; i <= N; i++){
+		if(knapsack_elements_greedy[i] != 0){
+			printf("%d ",knapsack_elements_greedy[i]);
+		}
 	}
 	printf("\n");
 }
@@ -143,7 +172,12 @@ int main(int argc,char **argv){
 			}	
 			if(line_number == 0){
 				printf("DP Solution : %d\n", dp_knapsack_solution(W,n));
-				print_knapsack_elements(get_knapsack_elements(W,n));
+				printf("Elements in the Knapsack due to DP Approach : ");
+				print_knapsack_elements_dp(get_knapsack_elements(W,n));
+				printf("Greedy Solution : %d\n",greedy_knapsack_solution(W,n));
+				printf("Elements in the Knapsack due to Greedy Approach : ");
+				print_knapsack_elements_greedy(num_elements);
+				printf("-------------------------------------------------------------------------------\n");
 			}
 		}
 	}
